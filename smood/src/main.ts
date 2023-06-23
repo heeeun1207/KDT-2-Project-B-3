ecddd7eaaa8a7ca108e26606575e5495832340c9
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-06-23 16:53:15 +0900

    main.ts에 미들웨어 추가

--- smood/src/main.ts
+++ smood/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+async function bootstrap(): Promise<NestExpressApplication> {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'client', 'build'));
+  return app;
+}
+async function start() {
+  const app = await bootstrap();
   await app.listen(3000);
 }
-bootstrap();
+start();
