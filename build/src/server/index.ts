f5656816a30b328ed15e1df8196928e5511b7f68
ellenjin51 <ellenjin51@gmail.com>
2023-06-23 11:54:13 +0900

    revised code again

--- build/src/server/index.ts
+++ build/src/server/index.ts
@@ -15,5 +15,5 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.listen(3000, () => {
-  console.log('Server is running on port 5000');
+  console.log('Server is running on port 3000');
 });
