ad6682999da8b32f6f470c8a1e2205610c8da64f
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-06-23 17:10:03 +0900

    프리티어 충돌 해결, hello react 출력 확인

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import { AppModule } from './app.module';
+async function bootstrap(): Promise<NestExpressApplication> {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '../', 'client', '/dist'));
+  return app;
+}
+async function start() {
+  const app = await bootstrap();
+  await app.listen(3000);
+}
+start();
