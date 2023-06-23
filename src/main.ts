64651efc46a5bbd2e4958b41e2eefe7b4071297f
JseopKim <jsub5646@gmail.com>
2023-06-23 17:50:06 +0900

    nestjs 에서 react 띄우기까지 성공

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'my-react', 'build'));
+  app.enableCors(); // 필요한 경우 CORS 설정
+  await app.listen(3000);
+}
+bootstrap();
