54a6559a87c59602892f7dd9d108de9d75b834f1
jungheeeun <jungheeeun272@gmail.com>
2023-06-23 16:40:46 +0900

    nestjs

---
+++ src/app.controller.ts
@@ -0,0 +1,9 @@
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
+@Controller('*')
+export class AppController {
+  @Get()
+  getIndex(@Res() res: Response) {
+    res.sendFile('index.html', { root: './client/build' });
+  }
+}
