c5c223284e5ca3f16536735a63c3e12f7e27816f
jungheeeun <jungheeeun272@gmail.com>
2023-06-27 12:46:46 +0900

    js 로 맵 가져오기

--- client/build/src/App.tsx
+++ client/build/src/App.tsx
@@ -1,34 +1,27 @@
 import React, { useEffect } from 'react';
-import './app.css';
-
-declare global {
-  interface Window {
-    kakao: any;
-  }
-}
 
 const App = () => {
   useEffect(() => {
     const script = document.createElement('script');
     script.async = true;
-    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=id=false`;
+    script.src = '/path/to/kakao/maps/sdk.js'; // 로컬 경로로 변경해주세요
 
     document.head.appendChild(script);
 
     script.onload = () => {
       window.kakao.maps.load(() => {
-        const mapContainer = document.getElementById('map');
-        const mapOptions = {
+        const container = document.getElementById('map');
+        const options = {
           center: new window.kakao.maps.LatLng(33.450701, 126.570667),
           level: 3,
         };
 
-        const map = new window.kakao.maps.Map(mapContainer, mapOptions);
+        new window.kakao.maps.Map(container, options);
       });
     };
   }, []);
 
-  return <div id="map" style={{ width: '100%', height: '400px' }} />;
+  return <div id="map" style={{ width: '500px', height: '400px' }} />;
 };
 
 export default App;
