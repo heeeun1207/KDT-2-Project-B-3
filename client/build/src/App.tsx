dccb3fff8940fc55b25b759e07b1980d3d3ed047
jungheeeun <jungheeeun272@gmail.com>
2023-06-27 12:00:12 +0900

    카카오 맵 API 지도

--- client/build/src/App.tsx
+++ client/build/src/App.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
-import "./test.css";
+import React, { useEffect } from 'react';
+import './app.css';
 
-const App = ():JSX.Element => {
-    return (
-        <>
-            <h1>Hello React</h1>
-        </>
-    );
+declare global {
+  interface Window {
+    kakao: any;
+  }
 }
+
+const App = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=id=false`;
+
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      window.kakao.maps.load(() => {
+        const mapContainer = document.getElementById('map');
+        const mapOptions = {
+          center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+          level: 3,
+        };
+
+        const map = new window.kakao.maps.Map(mapContainer, mapOptions);
+      });
+    };
+  }, []);
+
+  return <div id="map" style={{ width: '100%', height: '400px' }} />;
+};
 
 export default App;
