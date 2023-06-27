bdaebc09b1d33c7d7309bb36af3ce44cc8b2ce15
jungheeeun <jungheeeun272@gmail.com>
2023-06-27 14:51:04 +0900

    맵위에 마커와 인포윈도우 올리기

--- client/build/src/KakaoMap.tsx
+++ client/build/src/KakaoMap.tsx
@@ -8,7 +8,7 @@ const KakaoMap = function () {
       style={{ width: '100%', height: '360px' }}
     >
       <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
-        <div style={{ color: '#000' }}>Hello World!</div>
+        <div style={{ color: '#000' }}>맵위에 마커와 인포윈도우 올리기!</div>
       </MapMarker>
     </Map>
   );
