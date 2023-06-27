d595a0b9b0e6fc39579e1a3c5808a00c3469622f
jungheeeun <jungheeeun272@gmail.com>
2023-06-27 14:32:20 +0900

    sdk 설치하고 시도중

--- client/build/src/App.tsx
+++ client/build/src/App.tsx
@@ -1,27 +1,13 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import './test.css';
+import KakaoMap from './KakaoMap';
 
-const App = () => {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.async = true;
-    script.src = '/path/to/kakao/maps/sdk.js'; // 로컬 경로로 변경해주세요
-
-    document.head.appendChild(script);
-
-    script.onload = () => {
-      window.kakao.maps.load(() => {
-        const container = document.getElementById('map');
-        const options = {
-          center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-          level: 3,
-        };
-
-        new window.kakao.maps.Map(container, options);
-      });
-    };
-  }, []);
-
-  return <div id="map" style={{ width: '500px', height: '400px' }} />;
+const App = (): JSX.Element => {
+  return (
+    <>
+      <KakaoMap />
+    </>
+  );
 };
 
 export default App;
