818220836b009e4b32142d978a8dcf55c740673c
jungheeeun <jungheeeun272@gmail.com>
2023-06-27 17:02:00 +0900

    any...any... 키워드로 장소검색하기

--- client/build/src/KakaoMap.tsx
+++ client/build/src/KakaoMap.tsx
@@ -1,69 +1,72 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
 
-interface Position {
-  lat: number;
-  lng: number;
+interface Marker {
+  position: {
+    lat: number;
+    lng: number;
+  };
+  content: string;
 }
 
-const KakaoMap = function () {
-  const [position, setPosition] = useState<Position | null>(null);
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+export const KakaoMap = () => {
+  const [info, setInfo] = useState<Marker | null>(null);
+  const [markers, setMarkers] = useState<Marker[]>([]);
+  const [map, setMap] = useState<any>(null);
 
-  const handleMapClick = (_t: any, mouseEvent: any) => {
-    setPosition({
-      lat: mouseEvent.latLng.getLat(),
-      lng: mouseEvent.latLng.getLng(),
+  useEffect(() => {
+    if (!map) return;
+    const ps = new kakao.maps.services.Places();
+    const tempMarkers: Marker[] = [];
+
+    ps.keywordSearch('이태원 맛집', (data, status, _pagination) => {
+      if (status === kakao.maps.services.Status.OK) {
+        const bounds = new kakao.maps.LatLngBounds();
+
+        for (let i = 0; i < data.length; i++) {
+          tempMarkers.push({
+            position: {
+              lat: Number(data[i].y),
+              lng: Number(data[i].x),
+            },
+            content: data[i].place_name,
+          });
+          bounds.extend(
+            new kakao.maps.LatLng(Number(data[i].y), Number(data[i].x)),
+          );
+        }
+        setMarkers(tempMarkers);
+
+        map.setBounds(bounds);
+      }
     });
-  };
-
-  const handleMarkerMouseOver = () => {
-    setIsOpen(true);
-  };
-
-  const handleMarkerMouseOut = () => {
-    setIsOpen(false);
-  };
+  }, [map]);
 
   return (
-    <>
-      <Map
-        center={{
-          lat: 36.3489005,
-          lng: 127.37733,
-        }}
-        style={{
-          width: '100%',
-          height: '450px',
-        }}
-        level={3}
-        onClick={handleMapClick}
-      >
-        {position && <MapMarker position={position} />}
+    <Map
+      center={{
+        lat: 37.566826,
+        lng: 126.9786567,
+      }}
+      style={{
+        width: '100%',
+        height: '350px',
+      }}
+      level={3}
+      onCreate={setMap}
+    >
+      {markers.map((marker) => (
         <MapMarker
-          position={{
-            lat: 36.3489005,
-            lng: 127.37733,
-          }}
-          clickable={true}
-          onMouseOver={handleMarkerMouseOver}
-          onMouseOut={handleMarkerMouseOut}
+          key={`marker-${marker.content}-${marker.position.lat},${marker.position.lng}`}
+          position={marker.position}
+          onClick={() => setInfo(marker)}
         >
-          {isOpen && (
-            <div style={{ padding: '5px', color: '#000' }}>Hello World!</div>
+          {info && info.content === marker.content && (
+            <div style={{ color: '#000' }}>{marker.content}</div>
           )}
         </MapMarker>
-      </Map>
-      {position && (
-        <p>
-          {'클릭한 위치의 위도는 ' +
-            position.lat +
-            ' 이고, 경도는 ' +
-            position.lng +
-            ' 입니다'}
-        </p>
-      )}
-    </>
+      ))}
+    </Map>
   );
 };
 
