d03733468a3f5848966f77ae4b8be416444ae94b
jungheeeun <jungheeeun272@gmail.com>
2023-06-27 15:56:47 +0900

    클릭한 위치에 마커 표시하기

--- client/build/src/KakaoMap.tsx
+++ client/build/src/KakaoMap.tsx
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Map, MapMarker, MapTypeId } from 'react-kakao-maps-sdk';
 
 const KakaoMap = function () {
+  const [position, setPosition] = useState<null | { lat: number; lng: number }>(
+    null,
+  );
+
+  const handleMapClick = (_t: any, mouseEvent: any) => {
+    setPosition({
+      lat: mouseEvent.latLng.getLat(),
+      lng: mouseEvent.latLng.getLng(),
+    });
+  };
+
   return (
-    <Map
-      center={{ lat: 36.3489005, lng: 127.37733 }}
-      style={{ width: '100%', height: '360px' }}
-    >
-      <MapMarker position={{ lat: 36.3489005, lng: 127.37733 }}>
-        <div style={{ color: '#000' }}>오라클빌딩에 마커표시!</div>
-      </MapMarker>
-      {/*지도에 교통정보 표시하기*/}
-      <MapTypeId type={kakao.maps.MapTypeId.TRAFFIC} />
-    </Map>
+    <>
+      <Map
+        center={{
+          lat: 36.3489005,
+          lng: 127.37733,
+        }}
+        style={{
+          width: '100%',
+          height: '450px',
+        }}
+        level={3}
+        onClick={handleMapClick}
+      >
+        {position && <MapMarker position={position} />}
+      </Map>
+      {position && (
+        <p>
+          {'클릭한 위치의 위도는 ' +
+            position.lat +
+            ' 이고, 경도는 ' +
+            position.lng +
+            ' 입니다'}
+        </p>
+      )}
+    </>
   );
 };
 
