a1c425c0b0fa09c47cfe265d15c926f8bb357333
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-06-27 15:47:34 +0900

    타이틀 문자 변경 테스트

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLocation } from "react-router-dom";
 import Header from './components/layout/header';
-import TestPage from './components/pages/testPage';
 import "./test.css";
 const App = ():JSX.Element => {
+    const location = useLocation();
+    const [pageTitle, setPageTitle] = useState("");
+    React.useEffect(()=> {
+        switch (location.pathname) {
+            case "/" :
+                setPageTitle("테스트");
+            break;
+            case "/home" :
+                setPageTitle("홈")
+        }
+    }, [location.pathname])
     return (
         <>
         <div>
-        <Header title="My Header" />
-            <h1>Hello React</h1>
-            <TestPage />
+        <Header title={pageTitle} />
+            {/* <Routes>
+                <Route path='/' element={<TestPage />} />
+                <Route path='/home' element={<TestPage2 />} />
+            </Routes>
+        {[
+            "/",
+            "/home"
+        ].includes(location.pathname) ? null : (
+            <div>Loading...</div>
+        )} */}
         </div>
         </>
     );
