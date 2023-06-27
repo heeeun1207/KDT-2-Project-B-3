230390f8468a47dd95e2495c42c0b0948677b867
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-06-27 23:12:19 +0900

    폴백 모듈 제작 완료, 및 정상 작동 확인 -> 모든 get요청 index.html 반환

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
-import { FallbackControllerController } from './fallback-controller/fallback-controller.controller';
-import { FallbackModuleController } from './fallback-modules/fallback-module.controller';
 import { FallbackModuleModule } from './fallback-modules/fallback-module.module';
-import { FallbackModuleService } from './fallback-modules/fallback-module.service';
-import { FallbackControllerTsController } from './fallback.controller.ts/fallback.controller.ts.controller';
-
 
 @Module({
   imports: [FallbackModuleModule],
-  controllers: [ AppController, FallbackControllerController, FallbackControllerTsController, FallbackModuleController],
-  providers: [AppService, FallbackModuleService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/fallback-modules/fallback-module.module.ts
+++ src/fallback-modules/fallback-module.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { FallbakController } from './fallback-module.controller';
 import { FallbackModuleService } from './fallback-module.service';
 @Module({
-  controllers:[FallbackModuleService],
-  providers:[FallbakController]
+  controllers:[FallbakController],
+  providers:[FallbackModuleService]
 })
 export class FallbackModuleModule {}
