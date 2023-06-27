e004f1861b58d82397fb64d5824d828aad0a9711
jungheeeun <jungheeeun272@gmail.com>
2023-06-27 15:27:37 +0900

    오라클빌딩에 마커표시 -> sample기능들 살펴보기

--- client/build/src/KakaoMap.tsx
+++ client/build/src/KakaoMap.tsx
@@ -4,11 +4,11 @@ import { Map, MapMarker, MapTypeId } from 'react-kakao-maps-sdk';
 const KakaoMap = function () {
   return (
     <Map
-      center={{ lat: 33.5563, lng: 126.79581 }}
+      center={{ lat: 36.3489005, lng: 127.37733 }}
       style={{ width: '100%', height: '360px' }}
     >
-      <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
-        <div style={{ color: '#000' }}>맵위에 마커와 인포윈도우 올리기!</div>
+      <MapMarker position={{ lat: 36.3489005, lng: 127.37733 }}>
+        <div style={{ color: '#000' }}>오라클빌딩에 마커표시!</div>
       </MapMarker>
       {/*지도에 교통정보 표시하기*/}
       <MapTypeId type={kakao.maps.MapTypeId.TRAFFIC} />
