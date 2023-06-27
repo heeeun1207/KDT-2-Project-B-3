bfa927730aeacbca1fc7423410823b252fc45b51
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-06-27 14:27:57 +0900

    폴더링 작업

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CreateMapModuleController } from './create-map-module/create-map-module.controller';
+import { CreateMapModuleService } from './create-map-module/create-map-module.service';
+import { LoginModuleController } from './login-module/login-module.controller';
+
+import { LoginModuleService } from './login-module/login-module.service';
+
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, LoginModuleController, CreateMapModuleController, ],
+  providers: [AppService, CreateMapModuleService, LoginModuleService, ],
 })
 export class AppModule {}
