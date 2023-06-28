155dfd4e4c3392418ea23975a33b6d440eddc59d
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-06-28 11:54:09 +0900

    다시 root 경로로 이동

---
+++ client/service-worker.js
@@ -0,0 +1,20 @@
+serviceWorkerRegistration.register();
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('app-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        './index.html',
+        // 추가로 캐싱할 파일 경로
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
