858a693a4e98f8a3b9c6694ee8802228193a9721
jungheeeun <jungheeeun272@gmail.com>
2023-06-28 12:42:09 +0900

    사이즈조절

--- client/src/components/common/Search.tsx
+++ client/src/components/common/Search.tsx
@@ -2,23 +2,53 @@ import React from 'react';
 
 const Search = (): JSX.Element => {
   return (
-    <div>
+    <div
+      style={{
+        width: '273px',
+        height: '53px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        border: 'none',
+        backgroundColor: '#ffffff',
+        boxShadow: '0 0 4px rgba(0, 0, 0, 0.5)',
+        padding: '10px',
+        fontSize: '16px',
+        position: 'relative',
+      }}
+    >
       <input
         type="text"
         style={{
-          width: '270px',
-          height: '50px',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
+          width: '100%',
+          height: '100%',
           border: 'none',
-          backgroundColor: '#ffffff',
-          boxShadow: '0 0 4px rgba(0, 0, 0, 0.5)',
-          padding: '10px',
-          fontSize: '16px',
+          outline: 'none',
+          backgroundColor: 'transparent',
+          paddingLeft: '10px',
+          paddingRight: '30px',
+          fontSize: 'inherit',
         }}
         placeholder="Search..."
       />
+      <button
+        style={{
+          width: '53px',
+          height: '71px',
+          border: 'none',
+          background: 'none',
+          backgroundImage: 'url(images/Restroombutton.png)',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          backgroundColor: 'gray',
+          backgroundSize: 'cover',
+          position: 'absolute',
+          right: '10px',
+          top: '50%',
+          transform: 'translateY(-50%)',
+          cursor: 'pointer',
+        }}
+      ></button>
     </div>
   );
 };
