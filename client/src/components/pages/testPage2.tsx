0e226b1b59fb652003dbd5c3354c94ea76d8da4c
jungheeeun <jungheeeun272@gmail.com>
2023-06-28 16:48:51 +0900

    커스텀 버튼만들기

--- client/src/components/pages/testPage2.tsx
+++ client/src/components/pages/testPage2.tsx
@@ -1,34 +1,18 @@
 import React, { useState } from 'react';
-import Custombutton from '../services/Custombutton';
 import Search from '../common/Search';
+import ButtonList from '../services/ButtonList';
 
 const TestPage = (): JSX.Element => {
-  const [searchResults, setSearchResults] = useState<string[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>('');
 
-  const handleSearch = (searchTerm: string) => {
-    // Perform search logic based on the searchTerm
-    // Set the search results to the state
-    setSearchResults([searchTerm]);
-  };
-
-  const handleButtonClick = (buttonName: string) => {
-    console.log('Button clicked:', buttonName);
+  const handleSearch = (searchValue: string) => {
+    setSearchTerm(searchValue);
   };
 
   return (
     <div>
       <Search onSearch={handleSearch} />
-      {searchResults.map((result, index) => (
-        <Custombutton
-          key={index}
-          buttonInfo={{
-            value: result,
-            image: `${result.toLowerCase()}.jpg`,
-            name: result,
-          }}
-          onClick={() => handleButtonClick(result)}
-        />
-      ))}
+      <ButtonList searchTerm={searchTerm} />
     </div>
   );
 };
