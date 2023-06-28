81d95eae2e9030b48ae23280a81d02433c5ded2b
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-06-28 12:14:06 +0900

    리액트 파일 진입점에 서비스워커 관련 이벤트 추가

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,14 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import App from "./App";
-
-// ReactDOM.render(<App />, document.getElementById("root"));
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('Service Worker registered:', registration);
+      })
+      .catch(error => {
+        console.log('Service Worker registration failed:', error);
+      });
+  });
+}
 import React from "react";
 
 import { createRoot } from "react-dom/client";
