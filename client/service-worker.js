95b5836d3e46aea1bffecaf9d8d225d270939190
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-06-28 12:27:10 +0900

    serviceworker가 서버를 실행했을 때 리액트 정적파일과 넘어오는 것 확인

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,11 +1,29 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(registration => {
-        console.log('Service Worker registered:', registration);
-      })
-      .catch(error => {
-        console.log('Service Worker registration failed:', error);
-      });
-  });
-}
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('app-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/public/index.html',
+        '/client.bundle.js',
+      ]);
+    })
+  );
+});
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('app-cache').then(cache => {
+      return cache.addAll([
+        '/public/',
+        '/public/index.html',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  // 필요한 경우 활성화 이벤트를 처리할 수 있습니다.
+});
+
+self.addEventListener('fetch', event => {
+  // 필요한 경우 네트워크 요청을 가로채거나 처리할 수 있습니다.
+});
