84b789baef07ac65d77ddb0627efc4e45ccfdbf8
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-06-28 11:44:45 +0900

    serviceworker 제작

---
+++ client/service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('app-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        './public/index.html',
+        // 추가로 캐싱할 파일 경로
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
