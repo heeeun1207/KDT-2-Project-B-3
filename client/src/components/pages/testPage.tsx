52106e23ea6d313d28c4c782633b6d1cefacf39b
JseopKim <jsub5646@gmail.com>
2023-06-28 16:49:36 +0900

    지도에 마커 찍는것까지 성공

--- client/src/components/pages/testPage.tsx
+++ client/src/components/pages/testPage.tsx
@@ -9,20 +9,26 @@ declare global {
 const Map = () => {
   useEffect(() => {
     const initTmap = () => {
-      const map = new window.Tmapv2.Map("map_div", {
-        center: new window.Tmapv2.LatLng(37.566481622437934, 126.98502302169841),
-        width: "390px",
-        height: "844px",
-        zoom: 15
+      const map = new window.Tmapv2.Map('map_div', {
+        center: new window.Tmapv2.LatLng(
+          37.566481622437934,
+          126.98502302169841,
+        ),
+        width: '390px',
+        height: '844px',
+        zoom: 15,
+      });
+      //Marker 객체 생성.
+      let marker = new window.Tmapv2.Marker({
+        position: new window.Tmapv2.LatLng(37.5652045, 126.98602028), //Marker의 중심좌표 설정.
+        map: map, //Marker가 표시될 Map 설정..
       });
     };
 
     window.addEventListener('load', initTmap);
   }, []);
 
-  return (
-    <div id="map_div"></div>
-  );
+  return <div id="map_div"></div>;
 };
 
 export default Map;
