4a5de264c52715ea9ae1f65b9848360559363365
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-06-29 15:43:35 +0900

    커밋 버그 해결

--- client/src/components/pages/loginPage.tsx
+++ client/src/components/pages/loginPage.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import Login from '../services/login';
-const LoginPage = (): JSX.Element => {
-  return (
-    <Login/>
-  )
-  
+
+const Listdiv = (): JSX.Element => {
+  const style = {
+    width: '100%',
+    height: '300px',
+    backgroundColor: 'yellow',
+    border: '1px solid',
+  };
+
+  return <div style={style}></div>;
 };
 
-export default LoginPage;
+export default Listdiv;
