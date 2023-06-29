f80b3cafaa373bef4eaa12e69793b8e194951747
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-06-29 11:14:07 +0900

    정상적으로 오는 것 확인 + path에 띠라서 헤더 요소 바뀌는 것 확인

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,22 +7,15 @@ import "./test.css";
 const App = ():JSX.Element => {
     const location = useLocation();
     const [pageTitle, setPageTitle] = useState("");
-    interface TitleMapping {
-        [key: string]: string;
-    }
-    const titleMapping : TitleMapping = {
-        "/": "테스트",
-        "/home": "홈",
-    };
     React.useEffect(()=> {
-        // switch (location.pathname) {
-        //     case "/" :
-        //         setPageTitle("테스트");
-        //     break;
-        //     case "/home" :
-        //         setPageTitle("홈")
-        // }
-        setPageTitle(titleMapping[location.pathname] || "");
+        switch (location.pathname) {
+            case "/" :
+                setPageTitle("테스트");
+            break;
+            case "/home" :
+                setPageTitle("홈")
+        }
+        // setPageTitle(titleMapping[location.pathname] || "");
     }, [location.pathname])
     return (
         <div>
