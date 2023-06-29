c601eefafbb34fbe5de67a3dc85e12ce926c0b90
jungheeeun <jungheeeun272@gmail.com>
2023-06-29 11:07:13 +0900

    검색버튼 누르면 ListDiv 나타나게 하기

--- client/src/components/common/Search.tsx
+++ client/src/components/common/Search.tsx
@@ -1,60 +1,5 @@
-// import React from 'react';
-
-// const Search = (): JSX.Element => {
-//   return (
-//     <div
-//       style={{
-//         width: '273px',
-//         height: '53px',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         border: 'none',
-//         backgroundColor: '#ffffff',
-//         boxShadow: '0 0 4px rgba(0, 0, 0, 0.5)',
-//         padding: '10px',
-//         fontSize: '16px',
-//         position: 'relative',
-//       }}
-//     >
-//       <input
-//         type="text"
-//         style={{
-//           width: '100%',
-//           height: '100%',
-//           border: 'none',
-//           outline: 'none',
-//           backgroundColor: 'transparent',
-//           paddingLeft: '10px',
-//           paddingRight: '30px',
-//           fontSize: 'inherit',
-//         }}
-//         placeholder="Search..."
-//       />
-//       <button
-//         style={{
-//           width: '53px',
-//           height: '71px',
-//           border: 'none',
-//           background: 'none',
-//           backgroundImage: 'url(images/Restroombutton.png)',
-//           backgroundPosition: 'center',
-//           backgroundRepeat: 'no-repeat',
-//           backgroundColor: 'gray',
-//           backgroundSize: 'cover',
-//           position: 'absolute',
-//           right: '10px',
-//           top: '50%',
-//           transform: 'translateY(-50%)',
-//           cursor: 'pointer',
-//         }}
-//       ></button>
-//     </div>
-//   );
-// };
-
-// export default Search;
 import React, { useState } from 'react';
+import Listdiv from '../services/Listdiv';
 
 interface SearchProps {
   onSearch: (searchTerm: string) => void;
@@ -62,6 +7,7 @@ interface SearchProps {
 
 const Search: React.FC<SearchProps> = ({ onSearch }) => {
   const [searchTerm, setSearchTerm] = useState('');
+  const [showList, setShowList] = useState<string[]>([]);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
@@ -69,12 +15,53 @@ const Search: React.FC<SearchProps> = ({ onSearch }) => {
 
   const handleSearch = () => {
     onSearch(searchTerm);
+    setShowList((prevList) => [...prevList, searchTerm]);
   };
 
   return (
     <div>
-      <input type="text" value={searchTerm} onChange={handleInputChange} />
-      <button onClick={handleSearch}>검색</button>
+      <div
+        style={{
+          width: '273px',
+          height: '53px',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          border: 'none',
+          backgroundColor: '#ffffff',
+          boxShadow: '0 0 4px rgba(0, 0, 0, 0.5)',
+          padding: '10px',
+          fontSize: '16px',
+          position: 'relative',
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={handleInputChange}
+        />
+        <button
+          style={{
+            width: '53px',
+            height: '71px',
+            border: 'none',
+            background: 'none',
+            backgroundImage: 'url(images/Restroombutton.png)',
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: 'gray',
+            backgroundSize: 'cover',
+            position: 'absolute',
+            right: '10px',
+            top: '50%',
+            transform: 'translateY(-50%)',
+            cursor: 'pointer',
+          }}
+          onClick={handleSearch}
+        ></button>
+      </div>
+      {showList.length > 0 && <Listdiv />}
     </div>
   );
 };
--- client/src/components/services/Listdiv.tsx
+++ client/src/components/services/Listdiv.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Listdiv = (): JSX.Element => {
   const style = {
-    width: '100%',
+    width: '390px',
     height: '300px',
     backgroundColor: 'gray',
     border: '1px solid',
