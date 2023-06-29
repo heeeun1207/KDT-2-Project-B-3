f45062cfa583dcf88dc837610d1f3dc678e593d4
ellenjin51 <ellenjin51@gmail.com>
2023-06-29 12:52:07 +0900

    내용 적절히 수정 후 빌드 문제 해결

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,9 +3,11 @@ import { Route, Routes, useLocation } from 'react-router-dom';
 import Nav from './components/layout/footer';
 import Header from './components/layout/header';
 import ProtoTestPage from './components/pages/testPageHer';
-import Mainpage from './components/pages/Mainpage';
+// import Mainpage from './components/pages/Mainpage';
+import Mainpage from './components/pages/mainPage';
 import Custompage from './components/pages/Custompage';
-import Loginpage3 from './components/pages/Loginpage';
+// import Loginpage3 from './components/pages/Loginpage';
+import Listdiv from './components/services/Listdiv';
 // import TestPage2 from './components/pages/testPage2';
 import './test.css';
 const App = (): JSX.Element => {
@@ -36,7 +38,7 @@ const App = (): JSX.Element => {
         {/* <Route path="/test" element={<TestPage2 />} /> */}
         <Route path="/" element={<Mainpage />} />
         <Route path="/custom" element={<Custompage />} />
-        <Route path="/login" element={<Loginpage3 />} />
+        <Route path="/login" element={<Listdiv />} />
       </Routes>
     </div>
   );
