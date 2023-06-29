14dbe13c47b8ebe02b955c6dc8afa2d6556f72b8
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-06-29 11:11:27 +0900

    위험 요소 제거

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,14 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(registration => {
-        console.log('Service Worker registered:', registration);
-      })
-      .catch(error => {
-        console.log('Service Worker registration failed:', error);
-      });
-  });
-}
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('/service-worker.js')
+//       .then(registration => {
+//         console.log('Service Worker registered:', registration);
+//       })
+//       .catch(error => {
+//         console.log('Service Worker registration failed:', error);
+//       });
+//   });
+// }
 import React from "react";
 
 import { createRoot } from "react-dom/client";
