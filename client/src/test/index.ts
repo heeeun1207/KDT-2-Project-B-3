ad2d61d9fb6abd29de6e59edadc529377bd564d8
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-06-29 16:38:01 +0900

    미들웨어 적절히 수정하여 manifest.json와 service worker가져오는데 성공

--- client/src/test/index.ts
+++ client/src/test/index.ts
@@ -1,21 +1,21 @@
+import console from 'console';
 import express, { Request, Response } from 'express';
 import path from 'path';
+
 const app = express();
-const root = path.join(__dirname, "../")
-const rootPublic = path.join(__dirname, "../", "../", "public")
-console.log(root)
-console.log(rootPublic)
-// app.use(express.static(path.join(__dirname, './'), {
-//   setHeaders: (res, filePath) => {
-//     if (filePath.endsWith('.js')) {
-//       res.setHeader('Content-Type', 'application/javascript');
-//     }
-//   },
-// }));
-app.use(express.static(root));
+const root = path.join(__dirname, '..', '..');
+const rootPublic = path.join(root, '/public');
+
 app.use(express.static(rootPublic));
+
+// 서비스 워커 파일을 별도의 경로로 제공
+app.get('/service-worker.js', (req: Request, res: Response) => {
+  res.sendFile(path.join(rootPublic, 'service-worker.js'));
+});
+
+// 나머지 요청은 index.html 파일로 응답
 app.get('*', (req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, "../" ,"./index.html"));
+  res.sendFile(path.join(rootPublic, 'index.html'));
 });
 
 app.listen(3030, () => {
