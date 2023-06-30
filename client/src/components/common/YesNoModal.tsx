bd0b5d3a6c8591efbe0e0db8bd50675b2e8519a5
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-06-30 12:24:34 +0900

    한글로 변경

--- client/src/components/common/YesNoModal.tsx
+++ client/src/components/common/YesNoModal.tsx
@@ -16,8 +16,8 @@ const YesNoModal: React.FC<ConfirmationModalProps> = ({
       <div className="modal-content">
         <h2>Confirmation Modal</h2>
         <p>이 버튼을 편집하겠습니까?</p>
-        <button onClick={onConfirm}>Yes</button>
-        <button onClick={onClose}>No</button>
+        <button onClick={onConfirm}>예</button>
+        <button onClick={onClose}>아니오</button>
       </div>
     </div>
   );
