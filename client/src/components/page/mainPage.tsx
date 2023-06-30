db77e12e03dfa2535896e6cbe3d811fb09ae1139
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-06-30 11:50:51 +0900

    햄버거 메뉴 누르면 컴포넌트 불러오기

--- client/src/components/page/mainPage.tsx
+++ client/src/components/page/mainPage.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import MyInfo from '../common/myInfo';
 import Search from '../common/Search';
 import Gas_stationbutton from '../services/Gas_stationbutton';
 import Listdiv from '../services/Listdiv';
 import Local_convenience from '../services/Local_convenience';
 import Restroombutton from '../services/Restroombutton';
-
 const Mainpage = (): JSX.Element => {
   const handleSearch = (searchTerm: string) => {
     // Handle the search logic here
     console.log('Search term:', searchTerm);
   };
-
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <div>
       <Search onSearch={handleSearch} />
@@ -25,6 +28,10 @@ const Mainpage = (): JSX.Element => {
       </div>
       <div>
         <Listdiv />
+      </div>
+      <div>
+      <div onClick={toggleMenu}>햄버거 메뉴</div>
+      {isOpen && <MyInfo />}
       </div>
     </div>
   );
