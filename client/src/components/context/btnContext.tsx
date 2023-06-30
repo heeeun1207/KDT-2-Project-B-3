71f68ed162750913041ce8cd32e89bac23a40816
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-06-30 16:22:08 +0900

    디폴트 값이 될 버튼을 컨텍스트에 추가

--- client/src/components/context/btnContext.tsx
+++ client/src/components/context/btnContext.tsx
@@ -5,6 +5,21 @@ interface Button {
   image: string;
   name: string;
 }
+export const DefaultBtn = createContext<Button[]>([{
+  value: '편의점',
+  image: 'convenience-store.jpg',
+  name: '편의점',
+},
+{
+  value: '카페',
+  image: 'cafe.jpg',
+  name: '카페',
+},
+{
+  value: '주유소',
+  image: 'gas-station.jpg',
+  name: '주유소',
+}])
 export const BtnContext = createContext<Button[]>([
   {
     value: '편의점',
