21eb2ff45a2a6ac07695f17900b3fa9bb2250b7f
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-06-30 15:17:47 +0900

    컨텍스트 데이터 넘기는 것 확인

--- client/src/components/context/btnContext.tsx
+++ client/src/components/context/btnContext.tsx
@@ -1,4 +1,51 @@
 import { createContext } from 'react';
 // 최초 컨텍스트에 더미 데이터 넣음
-export const BtnContext = createContext<string>("테스트");
-export const BtnContextData = "하이"
+interface Button {
+  value: string;
+  image: string;
+  name: string;
+}
+export const BtnContext = createContext<Button[]>([
+  {
+    value: '편의점',
+    image: 'convenience-store.jpg',
+    name: '편의점',
+  },
+  {
+    value: '카페',
+    image: 'cafe.jpg',
+    name: '카페',
+  },
+  {
+    value: '주유소',
+    image: 'gas-station.jpg',
+    name: '주유소',
+  },
+  {
+    value: '화장실',
+    image: 'restroom.jpg',
+    name: '화장실',
+  },
+]);
+export const BtnContextData : Button[] = [
+  {
+    value: '편의점',
+    image: 'convenience-store.jpg',
+    name: '편의점',
+  },
+  {
+    value: '카페',
+    image: 'cafe.jpg',
+    name: '카페',
+  },
+  {
+    value: '주유소',
+    image: 'gas-station.jpg',
+    name: '주유소',
+  },
+  {
+    value: '화장실',
+    image: 'restroom.jpg',
+    name: '화장실',
+  },
+];
