03dfe965cefe1184e9dcb7e6e1639c284e1d1ec8
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-06-30 16:23:32 +0900

    사용하는 context를 디폴트 context로 변경

--- client/src/components/page/editButtonPage.tsx
+++ client/src/components/page/editButtonPage.tsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { useParams } from "react-router-dom";
-import EditButton from '../common/editButtonSelect';
-
+// import EditButton from '../common/editButtonSelect';
+import { DefaultBtn } from "../context/btnContext";
 const EditButtonPage: React.FC = () => {
+  const btnContextData = useContext(DefaultBtn)
+  console.log("자 두과자",btnContextData)
+  const [contextData, setContextData]= useState()
   const id = useParams();
-  console.log(id)
+  console.log("테스트",id)
+
+  const changeBtnData = ()=> {
+    
+  }
   return (<div>
     <p><strong>어떤 버튼에 적용하겠습니까?</strong></p>
 
-    <EditButton />
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
 
-    <button>확인</button>
+    <button onClick={changeBtnData}>확인</button>
 
   </div>)
 };
