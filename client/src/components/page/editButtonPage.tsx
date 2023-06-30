69cabdae226d1e6861f68627b1deba2d27a6c469
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-06-30 11:34:17 +0900

    editButtonPage로 컴포넌트 명 변경

--- client/src/components/page/editButtonPage.tsx
+++ client/src/components/page/editButtonPage.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import EditButton from '../common/editButtonSelect';
 
-const testPage = ():JSX.Element=> {
-  return <div>
+const editButtonPage = ():JSX.Element=> {
+  return (<div>
     <h3>단축 편집 버튼</h3>
     <p><strong>어떤 버튼에 적용하겠습니까?</strong></p>
 
@@ -10,6 +10,6 @@ const testPage = ():JSX.Element=> {
 
     <button>확인</button>
 
-  </div>
+  </div>)
 };
-export default testPage;
+export default editButtonPage;
