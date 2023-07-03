3e9397ea8cf5e48f2368047aa816b013f658050d
skybluejean <ellenjin51@gmail.com>
2023-07-03 19:43:41 +0900

    editPage 재수정

--- client/src/components/page/editButtonPage.tsx
+++ client/src/components/page/editButtonPage.tsx
@@ -14,17 +14,16 @@ const EditButtonPage: React.FC = () => {
   const navigate = useNavigate();
   const { btnContextData, updateBtnContext } = useContext(DefaultBtnContext);
   const [selectedButton, setSelectedButton] = useState<number>(-1);
+  // url 쿼리 데이터 객체에 담아 사용
   const { value, image, name } = useParams<ButtonParams>();
 
-  //? 데이터 못 가져왔을 때 예외처리
   if (!value || !image || !name) {
     // URL 파라미터가 없는 경우 처리
     return <div>URL 파라미터가 존재하지 않습니다.</div>;
   }
 
-  // 버튼 데이터 가지고 함수 실행
   const handleButtonSelect = (buttonIndex: number) => {
-    // 버튼 데이터를 handleConfirm()에서 사용
+    // 1, 2, 3번 state
     setSelectedButton(buttonIndex);
   };
 
@@ -32,10 +31,9 @@ const EditButtonPage: React.FC = () => {
     if (selectedButton !== -1) {
       // btnContextData의 n번째 데이터를 url로 받은 데이터로 재할당
       const updatedBtnContextData = btnContextData.map((button, index) => {
-        // 버튼 index와 데이터 index가 같으면 함수 실행
+        // 재할당 구간
         if (index === selectedButton) {
           //! 현재 이미지 URL을https://ko.imgbb.com/에서 생성해서 적용한 상태입니다. 리터럴로 되있는 상태인데 해결해야합니다
-          // 데이터 재할당
           const imageUrl = 'https://i.ibb.co/d6nBjHQ/cafe.png';
           return {
             value: value,
@@ -46,10 +44,8 @@ const EditButtonPage: React.FC = () => {
         return button;
       });
       alert('적용이 완료됐습니다.');
-      // updateBtnContext 메서드로 provider 데이터 업데이트
       updateBtnContext(updatedBtnContextData);
       // 변경후에 main페이지로 이동
-      navigate('/');
     }
   };
 
