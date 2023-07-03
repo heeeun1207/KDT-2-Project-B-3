dbb9bc8fa10bdf8467e094b1ac1d8a26a82abc0a
jungheeeun <jungheeeun272@gmail.com>
2023-07-03 12:18:40 +0900

    커스텀버튼 활성화 스타일 완료

--- client/src/components/services/Custombutton.tsx
+++ client/src/components/services/Custombutton.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../style/custombtn.css';
+
 interface ButtonInfo {
   image: string;
 }
@@ -11,12 +12,18 @@ interface CustomButtonProps {
 
 const CustomButton: React.FC<CustomButtonProps> = ({ buttonInfo, onClick }) => {
   const { image } = buttonInfo;
-  {
-    /* 커스텀 버튼 이미지만 보이도록 바꿨습니다. */
-  }
+  const [isActive, setIsActive] = useState(false);
+
+  const handleButtonClick = () => {
+    setIsActive(!isActive);
+    onClick();
+  };
 
   return (
-    <div className="button" onClick={onClick}>
+    <div
+      className={`button ${isActive ? 'active' : ''}`}
+      onClick={handleButtonClick}
+    >
       <img src={image} />
     </div>
   );
