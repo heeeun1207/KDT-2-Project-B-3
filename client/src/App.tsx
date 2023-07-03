1672f4e84be2092067f1044be69818d6d4eb4f7b
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-07-03 19:14:34 +0900

    필요 없는 파일 제거

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,13 +3,10 @@ import { Route, Routes, useLocation } from 'react-router-dom';
 import { BtnContext, BtnContextData, DefaultBtnContextProvider } from './components/context/btnContext';
 import Header from './components/layout/header';
 import Custompage from './components/page/custompage';
-import OpenMenuPage from './components/page/menuListPage';
 import EditButtonPage from './components/page/editButtonPage';
 import LoginPage from './components/page/loginPage';
 import Mainpage from './components/page/mainPage';
 // import TestPage from './components/page/testPage';
-import ProtoTestPage from './components/page/testPageHer';
-import MapPage from './components/page/mapPage';
 const App = (): JSX.Element => {
   console.log(BtnContextData);
   const location = useLocation();
@@ -18,22 +15,13 @@ const App = (): JSX.Element => {
     [key: string]: string;
   }
   const titleMapping: TitleMapping = {
-    '/': '',
-    '/main': '홈',
-    '/edit': '단축 편집 버튼',
+    '/': '홈',
+    '/edit': '사용자 버튼 편집',
     '/custom': '카테고리 선택',
     '/login': '로그인',
     '/signUp': '회원가입',
-    '/open': '햄버거 버튼 메뉴 선택'
   };
   React.useEffect(() => {
-    // switch (location.pathname) {
-    //     case "/" :
-    //         setPageTitle("테스트");
-    //     break;
-    //     case "/home" :
-    //         setPageTitle("홈")
-    // }
     setPageTitle(titleMapping[location.pathname] || '');
   }, [location.pathname]);
   return (
@@ -42,12 +30,9 @@ const App = (): JSX.Element => {
       <DefaultBtnContextProvider>
       <BtnContext.Provider value={BtnContextData}>
         <Routes>
-          <Route path="/" element={<ProtoTestPage />} />
-          <Route path="/main" element={<Mainpage />} />
-          <Route path="/map" element={<MapPage />} />
+          <Route path="/" element={<Mainpage />} />
           <Route path="/custom" element={<Custompage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/open" element={<OpenMenuPage />} />
           <Route path="/edit/:value/:image/:name" element={<EditButtonPage />} />
         </Routes>
       </BtnContext.Provider>
