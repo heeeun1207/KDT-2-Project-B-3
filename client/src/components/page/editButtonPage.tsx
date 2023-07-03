1bdf76c595e2749fe1ea412bb1cc162045970426
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-07-03 19:28:12 +0900

    설명 추가 및 필요없는 부분 제거

--- client/src/components/page/editButtonPage.tsx
+++ client/src/components/page/editButtonPage.tsx
@@ -10,45 +10,36 @@ interface ButtonParams {
   [key: string]: string;
 }
 
-export const EditButtonPage: React.FC = () => {
+const EditButtonPage: React.FC = () => {
   const navigate = useNavigate();
   // 기본 버튼 useContext
   const { btnContextData, updateBtnContext } = useContext(DefaultBtnContext);
 
-  // 컨텍스트 데이터 상태 관리
-  const [contextState, setContextState] = useState(btnContextData)
-  // 버튼 상태
-  // const [btnState, setBtnState]= useState(btnContextData);
-  // 버튼 상태 관리
+  // 1, 2, 3버튼 상태 관리
   const [selectedButton, setSelectedButton] = useState<number>(-1);
-  // url 쿼리 데이터 가져오기
+  // url 쿼리 데이터 객체에 담아 사용
   const { value, image, name } = useParams<ButtonParams>();
 
+  //? 데이터 못 가져왔을 때 예외처리
   if (!value || !image || !name) {
     // URL 파라미터가 없는 경우 처리
     return <div>URL 파라미터가 존재하지 않습니다.</div>;
   }
-  console.log('Value:', value);
-  console.log('Image:', image);
-  console.log('Name:', name);
 
+  // 버튼 데이터 가지고 함수 실행
   const handleButtonSelect = (buttonIndex: number) => {
-    // 1, 2, 3번 state
+    // 버튼 데이터를 handleConfirm()에서 사용
     setSelectedButton(buttonIndex);
   };
-
-  // 변경사항 확인하기 위한 콘솔
-  useEffect(() => {
-    console.log("안돼?", contextState);
-  }, [contextState]);
   
   const handleConfirm = () => {
     if (selectedButton !== -1) {
       // btnContextData의 n번째 데이터를 url로 받은 데이터로 재할당
       const updatedBtnContextData = btnContextData.map((button, index) => {
-        // 재할당 구간
+        // 버튼 index와 데이터 index가 같으면 함수 실행
         if (index === selectedButton) {
           //! 현재 이미지 URL을https://ko.imgbb.com/에서 생성해서 적용한 상태입니다. 리터럴로 되있는 상태인데 해결해야합니다
+          // 데이터 재할당
           const imageUrl = "https://i.ibb.co/d6nBjHQ/cafe.png" ;
           return {
             value: value,
@@ -60,15 +51,10 @@ export const EditButtonPage: React.FC = () => {
         return button;
       });
       alert("적용이 완료됐습니다.");
+      // updateBtnContext 메서드로 provider 데이터 업데이트
       updateBtnContext(updatedBtnContextData);
-      console.log("마지막 확인",updatedBtnContextData)
-      // setContextState(updatedBtnContextData);
-      navigate('/main');
-      
-    
-      
       // 변경후에 main페이지로 이동
-      
+      navigate('/main');
     }
   };
 
