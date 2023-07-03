85356890c8c673c938daa8e5a0db10b7fe217c3c
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-07-03 14:18:40 +0900

    컨텍스트 데이터 부분 수정 및 주석에 설명 첨부

--- client/src/components/context/btnContext.tsx
+++ client/src/components/context/btnContext.tsx
@@ -1,9 +1,9 @@
 import { createContext } from 'react';
 //src 안으로 images 폴더 넣었습니다.
-import convenienceStoreImage from '../images/convenience-store.png';
-import restroomImage from '../images/restroom.png';
-import gasStationImage from '../images/gas_station.png';
 import cafeImage from '../images/cafe.png';
+import convenienceStoreImage from '../images/convenience-store.png';
+import gasStationImage from '../images/gas_station.png';
+import restroomImage from '../images/restroom.png';
 
 // 최초 컨텍스트에 더미 데이터 넣음
 export interface Button {
@@ -11,8 +11,9 @@ export interface Button {
   image: string;
   name: string;
 }
-
-const DefaultBtnData = createContext<Button[]>([
+// ! 기본 버튼 데이터(3개)
+// 디폴트 데이터
+const DefaultBtnContextData: Button[] = [
   {
     value: '편의점',
     image: convenienceStoreImage,
@@ -28,31 +29,12 @@ const DefaultBtnData = createContext<Button[]>([
     image: gasStationImage,
     name: '주유소',
   },
-]);
+];
+  // 컨텍스트 API 생성
+const DefaultBtnData = createContext<Button[]>(DefaultBtnContextData);
 
-const BtnContext = createContext<Button[]>([
-  {
-    value: '편의점',
-    image: convenienceStoreImage,
-    name: '편의점',
-  },
-  {
-    value: '카페',
-    image: cafeImage,
-    name: '카페',
-  },
-  {
-    value: '주유소',
-    image: gasStationImage,
-    name: '주유소',
-  },
-  {
-    value: '화장실',
-    image: restroomImage,
-    name: '화장실',
-  },
-]);
-
+// ! 모든 버튼 데이터
+// 컨텍스트에 사용 할 데이터
 const BtnContextData: Button[] = [
   {
     value: '편의점',
@@ -75,5 +57,8 @@ const BtnContextData: Button[] = [
     name: '화장실',
   },
 ];
+// 컨텍스트 API 생성
+const BtnContext = createContext<Button[]>(BtnContextData);
 
 export { DefaultBtnData, BtnContext, BtnContextData };
+
