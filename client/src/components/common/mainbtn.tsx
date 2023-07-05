1539347f08bc83106b501ad1b9b510396c5d945b
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-07-05 10:37:28 +0900

    메인 버튼에 flex

--- client/src/components/common/mainbtn.tsx
+++ client/src/components/common/mainbtn.tsx
@@ -5,13 +5,11 @@ import '../style/mainbtn.css';
 const MainBtn = ({ handleBtnClick }: { handleBtnClick: (name: string) => void }): JSX.Element => {
   const { btnContextData } = useContext(DefaultBtnContext);
 
-
+// 메인 페이지에서 생성한 세개의 버튼입니다.
   return (
-    <div>
+    <div style={{display : 'flex', flexDirection : 'column', alignItems : 'center', justifyContent : 'space-around'}}>
       {btnContextData.map((data, index) => {
         const { name, image } = data;
-        console.log("이게 이미지 이름입니다",image);
-        console.log("이게 이름입니다", name);
         return (
           <div key={index}>
             <div className="main-button" onClick={() => handleBtnClick(name)}>
