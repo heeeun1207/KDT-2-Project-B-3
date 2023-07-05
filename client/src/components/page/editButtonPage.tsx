2f871f40d461eb420f5101f8112526c84b8a0a70
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-07-05 14:01:06 +0900

    정상적으로 동작하는 것 확인

--- client/src/components/page/editButtonPage.tsx
+++ client/src/components/page/editButtonPage.tsx
@@ -1,9 +1,8 @@
-import React, { useContext, useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-// import EditButton from '../common/editButtonSelect';
-import { DefaultBtnContext } from "../context/btnContext";
+import React, { useContext, useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { DefaultBtnContext } from '../context/btnContext';
 import '../style/editpage.css';
-// url로 가져온 데이터 인터페이스
+
 interface ButtonParams {
   value: string;
   image: string;
@@ -13,80 +12,79 @@ interface ButtonParams {
 
 const EditButtonPage: React.FC = () => {
   const navigate = useNavigate();
-  // 기본 버튼 useContext
   const { btnContextData, updateBtnContext } = useContext(DefaultBtnContext);
-
-  // 1, 2, 3버튼 상태 관리
   const [selectedButton, setSelectedButton] = useState<number>(-1);
   // url 쿼리 데이터 객체에 담아 사용
   const { value, image, name } = useParams<ButtonParams>();
 
-  //? 데이터 못 가져왔을 때 예외처리
   if (!value || !image || !name) {
     // URL 파라미터가 없는 경우 처리
     return <div>URL 파라미터가 존재하지 않습니다.</div>;
   }
 
-  // 버튼 데이터 가지고 함수 실행
   const handleButtonSelect = (buttonIndex: number) => {
-    // 버튼 데이터를 handleConfirm()에서 사용
+    // 1, 2, 3번 state
     setSelectedButton(buttonIndex);
   };
-  
+
   const handleConfirm = () => {
     if (selectedButton !== -1) {
       // btnContextData의 n번째 데이터를 url로 받은 데이터로 재할당
       const updatedBtnContextData = btnContextData.map((button, index) => {
-        // 버튼 index와 데이터 index가 같으면 함수 실행
+        // 재할당 구간
         if (index === selectedButton) {
           //! 현재 이미지 URL을https://ko.imgbb.com/에서 생성해서 적용한 상태입니다. 리터럴로 되있는 상태인데 해결해야합니다
-          // 데이터 재할당
-          const imageUrl = "https://i.ibb.co/d6nBjHQ/cafe.png" ;
+          const imageUrl = 'https://i.ibb.co/d6nBjHQ/cafe.png';
           return {
             value: value,
             image: imageUrl,
             name: name,
           };
         }
-        console.log("제발",button)
         return button;
       });
-      alert("적용이 완료됐습니다.");
-      // 변경후에 main페이지로 이동
-      navigate('/');
-      // updateBtnContext 메서드로 provider 데이터 업데이트
+      alert('적용이 완료됐습니다.');
+      navigate('/')
       updateBtnContext(updatedBtnContextData);
-    
+      // 변경후에 main페이지로 이동
     }
   };
 
   useEffect(() => {
-    setSelectedButton(0); // 첫 로딩시 0번 버튼에 CSS 적용
+    setSelectedButton(0);
   }, []);
 
   return (
-    <div style={{display : 'flex', flexDirection : 'row',
-    }}>
-    <p><strong>어떤 버튼에 적용하겠습니까?</strong></p>
-
-    <div className={selectedButton === 0 ? 'edit_btn selected' : 'edit_btn'}
-      onClick={() => handleButtonSelect(0)}
-    >
-      1
-    </div>
-    <div className={selectedButton === 1 ? 'edit_btn selected' : 'edit_btn'}
-      onClick={() => handleButtonSelect(1)}
-    >
-      2
-    </div>
-    <div className={selectedButton === 2 ? 'edit_btn selected' : 'edit_btn'}
-      onClick={() => handleButtonSelect(2)}
-    >
-      3
-    </div>
-
-    <button onClick={handleConfirm}>확인</button>
-  </div>
+    <>
+      <p className="center-align">
+        <strong>어떤 버튼에 적용하겠습니까?</strong>
+      </p>
+      <div className="edit_container">
+        <div
+          onClick={() => handleButtonSelect(0)}
+          className={selectedButton === 0 ? 'edit_btn selected' : 'edit_btn'}
+        >
+          1
+        </div>
+        <div
+          onClick={() => handleButtonSelect(1)}
+          className={selectedButton === 1 ? 'edit_btn selected' : 'edit_btn'}
+        >
+          2
+        </div>
+        <div
+          onClick={() => handleButtonSelect(2)}
+          className={selectedButton === 2 ? 'edit_btn selected' : 'edit_btn'}
+        >
+          3
+        </div>
+      </div>
+      <div>
+        <button className="confirm" onClick={handleConfirm}>
+          확인
+        </button>
+      </div>
+    </>
   );
 };
 
